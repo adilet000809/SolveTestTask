ee1d6a0e29198dadd6b3283c83af762e77de57ca
Adilet Bolatbek <a.bolatbek2000@gmail.com>
2024-10-30 10:13:25 +0500

    Initial commit

---
+++ src/test/java/com/example/solvetesttask/SolveTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solvetesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolveTestTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
